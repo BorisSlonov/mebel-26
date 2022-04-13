ba557c80e8580b95a08882f911b641789b0bbdcb
Boris <sslonovborisss@gmail.com>
2022-04-13 22:00:17 +0300

    dell com

--- src/blocks/modules/popups/popups.js
+++ src/blocks/modules/popups/popups.js
@@ -1,8 +1,8 @@
 import $ from "jquery";
 import { Fancybox } from "@fancyapps/ui";
 
-// $(function () {
-//   if ($(window).width() >= 768) {
-//     window.location.replace("https://mebel-26.ru/");
-//   }
-// });
+$(function () {
+  if ($(window).width() >= 768) {
+    window.location.replace("https://mebel-26.ru/");
+  }
+});
